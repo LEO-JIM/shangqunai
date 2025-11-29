05d2a2c5a2ee2c2356d72d5a7708d0d972894774
LEO-JIM <lishy.leo1@gmail.com>
2025-11-29 17:59:59 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 import DynamicWords from "@/components/DynamicWords";
 import Header from "@/components/Header"; 
 import BackgroundNoise from "@/components/BackgroundNoise";
